daaa4dbfd10bac0bf4e9abe7260f5046848b4aa7
joaoaugustoma <joaoaugustoananias@gmail.com>
2022-10-18 20:26:31 -0300

    Initial Commit

---
+++ src/main/java/com/ueg/nutshellfrontend/application/AppStartupRunner.java
@@ -0,0 +1,11 @@
+package com.ueg.nutshellfrontend.application;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class AppStartupRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/ueg/nutshellfrontend/application/NutshellFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.ueg.nutshellfrontend.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutshellFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutshellFrontendApplication.class, args);
+	}
+
+}
